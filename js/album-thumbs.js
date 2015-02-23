0c8775c654529ded4771a5c201645dc3363a7af9
Brian Leonard <brian@theleoreport.com>
2015-02-22 20:34:46 -0500

    album thumbs js render and added to index

---
+++ js/album-thumbs.js
@@ -0,0 +1,19 @@
+var AlbumThumbs = (function(){
+
+  var template = JST["album-thumbs"];
+
+  function AlbumThumbs(data){
+    this.data = data;
+  }
+
+  AlbumThumbs.prototype = {
+    
+    render: function() {
+      return $(template(this.data));
+    }
+
+  }
+
+  return AlbumThumbs;
+
+})();
