3d405da59b4428f3974a430e9253f5f541a74ce7
Brian Leonard <brian@theleoreport.com>
2015-02-22 20:09:25 -0500

    starting over

--- js/app.js
+++ js/app.js
@@ -3,30 +3,11 @@ var App = (function(){
   function App(data) {
 
     this.data = data;
-
-    this.$homecontent = $(".home-content");
-
-    this.showGroups();
+    
 
   }
 
   App.prototype = {
-
-    getGroupNames: function() {
-      return _.chain(this.data)
-        .pluck("album")
-        .uniq()
-        .map(function(groupName){
-          return {album: groupName};
-        })
-        .value();
-    },
-
-    showGroups: function() {
-      var groupData = this.getGroupNames();
-      this.albumGroupList = new AlbumGroupList(groupData);
-      this.$homecontent.html( this.albumGroupList.render() );
-    }
     
   };
 
